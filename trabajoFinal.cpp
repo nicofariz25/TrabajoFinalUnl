2c775885782bd0d5a726a55f9f02279ed9b78f64
nicofariz25 <nicofariz76@gmail.com>
2026-02-12 13:42:56 -0300

    cree la clase Nave

--- trabajoFinal.cpp
+++ trabajoFinal.cpp
@@ -2,6 +2,44 @@
 #include <conio2.h>
 using namespace std;
 
+
+
+
+class Nave
+{
+private:
+	int dx;
+	int dy;
+public:
+	Nave();
+	void moverIzquierda();
+	void moverDerecha();
+	void dibujar();
+	void borrar();
+}
+Nave::Nave()
+{
+	dx = 25;
+	dy = 25;
+}
+void Nave::moverIzquierda()
+{
+	dx -= 1;
+}
+void Nave::moverDerecha()
+{
+	dx += 1;
+}
+void Nave::dibujar()
+{
+	gotoxy(dx,dy);
+	cout<<"A";
+}
+void Nave::borrar()
+{
+	cout<<" ";
+}
+
 int main(int argc, char *argv[]) {
 	
 	return 0;
